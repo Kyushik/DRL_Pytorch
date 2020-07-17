491ef250aa3d6d95b0efc01a7b356ccf8c83cf53
Dongchan Kim <jookker@hanyang.ac.kr>
2020-07-17 17:43:37 +0900

    Add batchnorm to DQN model

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Config
 import datetime
 
-state_size = [80,80,3]
-action_size = 4
+state_size = [80,80,1]
+action_size = 3
 
 load_model = False
 train_mode = True
@@ -10,7 +10,7 @@ train_mode = True
 batch_size = 32
 mem_maxlen = 50000
 
-discount_factor = 0.9
+discount_factor = 0.99
 learning_rate = 0.00025
 
 skip_frame = 4
@@ -18,7 +18,7 @@ stack_frame = 4
 
 start_train_step = 10000
 run_step = 50000
-test_step = 2500
+test_step = 25000
 
 target_update_step = int(run_step/100)
 print_episode = 10
@@ -28,15 +28,15 @@ epsilon_init = 1.0
 epsilon_min = 0.1
 
 # Environment Setting
-env_config = {'gridSize':3}
-# env_config = {}
+# env_config = {'gridSize':3}
+env_config = {}
 
 # Environment Path
-game = "GridWorld"
+game = "Breakout"
 env_name = "./env/" + game + "/Windows/" + game
 
 # 모델 저장 및 불러오기 경로
 date_time = datetime.datetime.now().strftime("%Y%m%d-%H-%M-%S")
 
 save_path = "./saved_models/" + game + "/" + date_time
-load_path = "./saved_models/" + game + "/20200713-18-46-18_DQN/model.pth"
+load_path = "./saved_models/" + game + "/20200716-20-32-00_NoisyDQN/model.pth"
--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import config
 
 import numpy as np
+import math
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -13,21 +14,23 @@ class DQN(nn.Module):
         super(DQN, self).__init__()
         input_channel = config.state_size[2]*config.stack_frame
         self.conv1 = nn.Conv2d(in_channels=input_channel, out_channels=32, kernel_size=8, stride=4, padding=4)
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=2)
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
         self.fc1 = nn.Linear(64*int(config.state_size[0]/8)*int(config.state_size[1]/8), 512)
         self.fc2 = nn.Linear(512, num_action)
 
     def forward(self, x):
         x = (x-(255.0/2))/(255.0/2)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
         x = x.view(-1, 64*int(config.state_size[0]/8)*int(config.state_size[1]/8))
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-
 
 class DuelingDQN(nn.Module):
     def __init__(self, num_action, network_name):
@@ -60,44 +63,110 @@ class DuelingDQN(nn.Module):
 
 
 class NoisyDQN(nn.Module):
-    def __init__(self, num_action, network_name):
+    def __init__(self, num_action, model_name, device):
         super(NoisyDQN, self).__init__()
         input_channel = config.state_size[2]*config.stack_frame
         self.conv1 = nn.Conv2d(in_channels=input_channel, out_channels=32, kernel_size=8, stride=4, padding=4)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=2)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=1)
-
-        flat_size = 64*int(config.state_size[0]/8)*int(config.state_size[1]/8)
-        self.mu_w1 = nn.init.uniform_(torch.empty(flat_size, 512), -np.sqrt(3/flat_size), np.sqrt(3/flat_size)).to(device)
-        self.sig_w1 = nn.init.constant_(torch.empty(flat_size, 512), 0.017).to(device)
-        self.mu_b1 = nn.init.uniform_(torch.empty(512), -np.sqrt(3/flat_size), np.sqrt(3/flat_size)).to(device)
-        self.sig_b1 = nn.init.constant_(torch.empty(512), 0.017).to(device)
-
-        self.mu_w2= nn.init.uniform_(torch.empty(512, num_action), -np.sqrt(3/512), np.sqrt(3/512)).to(device)
-        self.sig_w2 = nn.init.constant_(torch.empty(512, num_action), 0.017).to(device)
-        self.mu_b2 = nn.init.uniform_(torch.empty(num_action), -np.sqrt(3/512), np.sqrt(3/512)).to(device)
-        self.sig_b2 = nn.init.constant_(torch.empty(num_action), 0.017).to(device)
+        self.noisylinear1 = NoisyLinear(64*int(config.state_size[0]/8)*int(config.state_size[1]/8), 512, device)
+        self.noisylinear2 = NoisyLinear(512, num_action, device)
 
     def forward(self, x, is_train):
         x = (x-(255.0/2))/(255.0/2)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 64*int(config.state_size[0]/8)*int(config.state_size[1]/8))
+        x = F.relu(self.conv3(x)) # [bs:32, 64, 10, 10]
+        x = x.view(x.size(0), -1) # [32, 6400]
+        x = F.relu(self.noisylinear1(x, is_train))
+        out = self.noisylinear2(x, is_train)
+        return out
 
-        x = F.relu(self.noisy_dense(x, self.mu_w1, self.sig_w1, self.mu_b1, self.sig_b1, is_train))
-        x = self.noisy_dense(x, self.mu_w2, self.sig_w2, self.mu_b2, self.sig_b2, is_train)
-        return x
+class NoisyLinear(nn.Module):
+    def __init__(self, in_nl, out_nl):
+        super(NoisyLinear, self).__init__()
+        self.in_nl = in_nl
+        self.out_nl = out_nl
+        self.std_init = 0.017
 
-    def noisy_dense(self, x, mu_w, sig_w, mu_b, sig_b, is_train):
+        # weight related variables
+        self.w_mu = nn.Parameter(torch.empty(self.out_nl, self.in_nl)).to(device)
+        self.w_sigma = nn.Parameter(torch.empty(self.out_nl, self.in_nl)).to(device)
+        self.w_eps = torch.empty(self.out_nl, self.in_nl).to(device)
+
+        # bias related variables
+        self.b_mu = nn.Parameter(torch.empty(self.out_nl)).to(device)
+        self.b_sigma = nn.Parameter(torch.empty(self.out_nl)).to(device)
+        self.b_eps = torch.empty(self.out_nl).to(device)
+
+        self.init_params()
+        self.init_noise()
+
+    def forward(self, x, is_train):
+        self.init_noise()
+        # print(self.w_sigma.data[0][0])
+
         if is_train:
-            eps_w = torch.randn(sig_w.size()).to(device)
-            eps_b = torch.randn(sig_b.size()).to(device)
+            w = self.w_mu.data + torch.mul(self.w_sigma.data, self.w_eps)
+            b = self.b_mu.data + torch.mul(self.b_sigma.data, self.b_eps)
         else:
-            eps_w = torch.zeros_like(sig_w).to(device)
-            eps_b = torch.zeros_like(sig_b).to(device)
+            w = self.w_mu.data
+            b = self.b_mu.data
+        return F.linear(x, w, b)
 
-        w_fc = mu_w + (sig_w * eps_w)
-        b_fc = mu_b + (sig_b * eps_b)
 
-        return torch.matmul(x, w_fc) + b_fc
+    def init_params(self):
+        mu_dist_range = math.sqrt(3/self.in_nl)
+        self.w_mu.data.uniform_(-mu_dist_range, mu_dist_range)
+        self.b_mu.data.uniform_(-mu_dist_range, mu_dist_range)
+        self.w_sigma.data.fill_(self.std_init)
+        self.b_sigma.data.fill_(self.std_init)
+
+    def init_noise(self):
+        self.w_eps = torch.normal(mean=0.0, std=1.0, size=self.w_mu.size()).to(device)
+        self.b_pes = torch.normal(mean=0.0, std=1.0, size=self.b_mu.size()).to(device)
+
+
+#
+# class NoisyDQN(nn.Module):
+#     def __init__(self, num_action, network_name):
+#         super(NoisyDQN, self).__init__()
+#         input_channel = config.state_size[2]*config.stack_frame
+#         self.conv1 = nn.Conv2d(in_channels=input_channel, out_channels=32, kernel_size=8, stride=4, padding=4)
+#         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=2)
+#         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=1)
+#
+#         flat_size = 64*int(config.state_size[0]/8)*int(config.state_size[1]/8)
+#         self.mu_w1 = nn.init.uniform_(torch.empty(flat_size, 512), -np.sqrt(3/flat_size), np.sqrt(3/flat_size)).to(device)
+#         self.sig_w1 = nn.init.constant_(torch.empty(flat_size, 512), 0.017).to(device)
+#         self.mu_b1 = nn.init.uniform_(torch.empty(512), -np.sqrt(3/flat_size), np.sqrt(3/flat_size)).to(device)
+#         self.sig_b1 = nn.init.constant_(torch.empty(512), 0.017).to(device)
+#
+#         self.mu_w2= nn.init.uniform_(torch.empty(512, num_action), -np.sqrt(3/512), np.sqrt(3/512)).to(device)
+#         self.sig_w2 = nn.init.constant_(torch.empty(512, num_action), 0.017).to(device)
+#         self.mu_b2 = nn.init.uniform_(torch.empty(num_action), -np.sqrt(3/512), np.sqrt(3/512)).to(device)
+#         self.sig_b2 = nn.init.constant_(torch.empty(num_action), 0.017).to(device)
+#
+#     def forward(self, x, is_train):
+#         x = (x-(255.0/2))/(255.0/2)
+#         x = F.relu(self.conv1(x))
+#         x = F.relu(self.conv2(x))
+#         x = F.relu(self.conv3(x))
+#         x = x.view(-1, 64*int(config.state_size[0]/8)*int(config.state_size[1]/8))
+#
+#         x = F.relu(self.noisy_dense(x, self.mu_w1, self.sig_w1, self.mu_b1, self.sig_b1, is_train))
+#         x = self.noisy_dense(x, self.mu_w2, self.sig_w2, self.mu_b2, self.sig_b2, is_train)
+#         return x
+#
+#     def noisy_dense(self, x, mu_w, sig_w, mu_b, sig_b, is_train):
+#         if is_train:
+#             eps_w = torch.randn(sig_w.size()).to(device)
+#             eps_b = torch.randn(sig_b.size()).to(device)
+#         else:
+#             eps_w = torch.zeros_like(sig_w).to(device)
+#             eps_b = torch.zeros_like(sig_b).to(device)
+#
+#         w_fc = mu_w + (sig_w * eps_w)
+#         b_fc = mu_b + (sig_b * eps_b)
+#
+#         return torch.matmul(x, w_fc) + b_fc
