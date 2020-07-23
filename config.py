9baa69cc54be28ff6d0527b3fe3d37a2564ba2d4
Kyushik <kyushimin@gmail.com>
2020-07-23 13:15:11 +0900

    Update noisy, Add Drone env

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Config
 import datetime
 
-state_size = [40,80,1]
-action_size = 3
+state_size = [80,80,3]
+action_size = 4
 
 load_model = False
 train_mode = True
@@ -13,11 +13,11 @@ mem_maxlen = 50000
 discount_factor = 0.99
 learning_rate = 0.00025
 
-skip_frame = 4
-stack_frame = 4
+skip_frame = 1
+stack_frame = 1
 
 start_train_step = 10000
-run_step = 50000
+run_step = 250000
 test_step = 25000
 
 target_update_step = int(run_step/100)
@@ -32,11 +32,11 @@ epsilon_min = 0.1
 env_config = {}
 
 # Environment Path
-game = "Pong"
-env_name = "./env/" + game + "/Linux/" + game
+game = "GridWorld"
+env_name = "./env/" + game + "/Windows/" + game
 
 # 모델 저장 및 불러오기 경로
 date_time = datetime.datetime.now().strftime("%Y%m%d-%H-%M-%S")
 
 save_path = "./saved_models/" + game + "/" + date_time
-load_path = "./saved_models/" + game + "/20200716-20-32-00_NoisyDQN/model.pth"
+load_path = "./saved_models/" + game + "/20200721-20-44-39_DoubleDQN/model.pth"
